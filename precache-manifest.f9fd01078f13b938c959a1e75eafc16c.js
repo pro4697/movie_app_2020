5356bbd0eae22bde1920bbf1fa9e2a3e8bc420c1
GitChani <pro4697@naver.com>
2020-06-29 17:46:35 +0900

    Updates

---
+++ precache-manifest.f9fd01078f13b938c959a1e75eafc16c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca47f7a2526f262f9ed1cb598ed3e3f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "588963ef7c26202bc77d",
+    "url": "/movie_app_2020/static/css/main.ce42ade3.chunk.css"
+  },
+  {
+    "revision": "15602e1df3c1913c6b40",
+    "url": "/movie_app_2020/static/js/2.187f6c70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.187f6c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588963ef7c26202bc77d",
+    "url": "/movie_app_2020/static/js/main.52e8e042.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
